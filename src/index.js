9c22196a5db19d383b4c643bf00e11d126105513
yassermorteza <yasser.morteza@gmail.com>
2018-02-20 19:28:07 +0100

    push al files

--- src/index.js
+++ src/index.js
@@ -1,157 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = function createMatrix(rowsCount, columnsCount){
- var matrix =[];
-
- for(var i=0; i<rowsCount; i++ ){
-    matrix[i]=[];
-    for(var x=0; x<columnsCount; x++){
-     matrix[i].push(0);
-    }
-  }
-return matrix;
-}
-
-},{}],2:[function(require,module,exports){
-module.exports = {
-  createMatrix: require('./create-matrix'),
-  matrixFillColNum: require('./matrix-fill-col-num'),
-  matrixFillRowNum: require('./matrix-fill-row-num'),
-  matrixFillSequence: require('./matrix-fill-sequence'),
-  matrixFillText: require('./matrix-fill-text'),
-  matrixIncrement: require('./matrix-increment'),
-  renderHtmlTable: require('./render-html-table'),
-  renderPlainText: require('./render-plain-text'),
-  renderTableDom: require('./render-table-dom')
-}
-
-
-},{"./create-matrix":1,"./matrix-fill-col-num":3,"./matrix-fill-row-num":4,"./matrix-fill-sequence":5,"./matrix-fill-text":6,"./matrix-increment":7,"./render-html-table":8,"./render-plain-text":9,"./render-table-dom":10}],3:[function(require,module,exports){
-module.exports = function matrixFillColNum(data){
-  var matrixx=[];
-  var counter=0;
-  data.forEach(function(col,index){
-     matrixx[index]=[];
-     for(var i=0; i< data[index].length; i++){
-       matrixx[index].push(counter+i);
-     }
-  })
-  return matrixx;
-}
-
-},{}],4:[function(require,module,exports){
-module.exports = function matrixFillRowNum (data){
-
-  var matrixx=[];
-
-  data.forEach(function(rows, index){
-    matrixx[index]= [];
-    for(var i=0; i<data[index].length; i++){
-      matrixx[index].push(index);
-    }
-  })
-  return matrixx;
-}
-
-},{}],5:[function(require,module,exports){
-module.exports = function matrixFillsequence(data){
-
-    var newmatrixx= [];
-    var counter = 0;
-    for(var rownum=0; rownum < data.length ; rownum++){
-
-      newmatrixx[rownum] = [];
-      var columnscount= data[rownum].length;
-
-      for(var i=0 ; i < columnscount ; i++){
-        newmatrixx[rownum].push(counter);
-        counter++;
-      }
-    }
-    return newmatrixx;
-}
-},{}],6:[function(require,module,exports){
-module.exports = function matrixFillText(data){
-
-  var text = 'abcdefghijklmnopqrstuvwxyz';
-  var strToArr = text.split('');
-  var newmatrixx = [];
-  counter = 0;
-
-  data.forEach(function(txt, index){
-
-    newmatrixx[index]=[];
-
-    for(var i=0; i<data[index].length; i++){
-      newmatrixx[index].push(strToArr[counter]);
-      counter++;
-    }
-  })
-  return newmatrixx;
-}
-
-},{}],7:[function(require,module,exports){
-module.exports = function matrixIncrement(data){
-
-  var newmatrixx = [];
-  var counter = 0;
-  data.forEach(function(increment, index){
-    newmatrixx[index]=[];
-    for(var i=0; i < data[index].length; i++){
-      counter++;
-      newmatrixx[index].push(counter);
-    }
-  })
-  return newmatrixx;
-}
-},{}],8:[function(require,module,exports){
-module.exports = function renderHTMLTable(matrix){
-
-    var text = '';
-
-    matrix.forEach(function(el){
-      text += "<tr>";
-
-      for(var i=0; i< el.length; i++){
-         text += '<td>' + el[i] + "</td>";
-      }
-
-      text += "</tr>";
-    })
-
-    return text;
-}
-},{}],9:[function(require,module,exports){
-module.exports = function renderPlainText(data){
-
-    var  rows =[];
-    data.forEach(function(columns){
-      var row ='';
-      row = columns.join('') + "<br>";
-      rows.push(row);
-    });
-   var result = rows.join('\n')
-  return result;
-}
-
-},{}],10:[function(require,module,exports){
-module.exports = function renderTabelDom(element, data){
-
-
-  data.forEach(function(columns, index){
-
-     var row = document.createElement('tr');
-
-     for(var i=0; i < data[index].length; i++){
-
-      var col = document.createElement('td');
-      col.appendChild(document.createTextNode(data[index][i]));
-      // col.textContent=data[index][i];
-      row.appendChild(col);
-     }
-     element.appendChild(row);
-  })
-}
-},{}],11:[function(require,module,exports){
 var arrayMatrixLib = require('array-matrix');
 var createMatrix = arrayMatrixLib.createMatrix;
 var matrix = createMatrix(3, 3);
@@ -248,8 +94,6 @@ function checkWinner(ev) {
         (row + cell === 2) ? x.diagonal2++: false;
         x.storeRow.push(x.row0, x.row1, x.row2, x.diagonal1);
         x.storeCell.push(x.cell0, x.cell1, x.cell2, x.diagonal2);
-        console.log(x.storeRow);
-        console.log(x.storeCell);
         if (x.storeRow.includes(3) || x.storeCell.includes(3)) {
             e.divWiner.textContent = "X won the game !!";
             turn = "X";
@@ -321,5 +165,50 @@ e.btn.addEventListener('click', () => {
     e.div.textContent = "X starts";
 });
 
+// conditoin('cross', ev, "X", "O", x.row0, x.row1, x.row2, x.cell0, x.cell1, x.cell2,
+//             x.diagonal1, x.diagonal2, x.storeRow, x.storeCell);
 
-},{"array-matrix":2}]},{},[11]);
+// function conditoin(classCss, ev, p1, p2, r0, r1, r2, c0, c1, c2, d1, d2, stw, stl) {
+
+//     ev.target.classList.add(classCss);
+//     var cell = ev.target.cellIndex;
+//     var row = ev.target.parentElement.rowIndex;
+//     e.div.textContent = "It's " + p2 + " Turn";
+//     turn = p2;
+//     (row === 0) ? r0++ : (row === 1) ? r1++ : r2++;
+//     (cell === 0) ? c0++ : (cell === 1) ? c1++ : c2++;
+//     (row === cell) ? d1++ : false;
+//     (row + cell === 2) ? d2++ : false;
+//     stw.push(r0, r1, r2, d1);
+//     stl.push(c0, c1, c2, d2);
+//     console.log(stw);
+//     console.log(stl);
+//     if (stw.includes(3) || stl.includes(3)) {
+//         e.divWiner.textContent = p1 + "  won the game !!";
+//         turn = p1;
+//         e.div.textContent = "It's " + p1 + "  Turn";
+//         counterX++;
+//         e.spanX.textContent = counterX;
+//         e.divPopUp.textContent = "paly again!";
+//         popUp();
+//     } else if (stw.length === 36 && !stw.includes(3) && !stl.includes(3)) {
+//         e.divPopUp.textContent = "No winner!";
+//         popUp();
+//     }
+
+// };
+// Error {
+//     [0, 0, 0, 2, 1, 0]
+//     [0, 0, 0, 1, 1, 1],
+//     [0, 0, 1, 0, 1, 1],
+//     [1, 0, 0, 0, 0, 2],
+//     [1, 0, 0, 2, 0, 0],
+//     [0, 2, 0, 0, 1, 0],
+//}
+
+//for (var item of storeCellX) {
+// var sum = storeCellX.reduce((sum, num) => sum + num);
+// console.log(sum);
+// (sum % 3 === 0) ? console.log('winner'): console.log('Failed');
+// //}
+//}
